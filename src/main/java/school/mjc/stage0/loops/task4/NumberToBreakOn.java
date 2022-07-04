d9bb0119e23edf30b747928c3918a44c6247727d
Abduvali Aminov <aminov.abduvali.98@gmail.com>
2022-07-04 18:46:01 +0500

    fixed

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
-
+        if (toBreakWith > numberToGoUntil){
+            System.out.print("iterating till the end");
+        }else{
+            for (int i = 0; i < numberToGoUntil; i++){
+                System.out.println(i+1);
+                if (i+1 == toBreakWith) break;
+            }
+        }
     }
 }
